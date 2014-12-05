4dab6161954993fe75f5628bb6504fc8c80f2e4c
oujf <cqojf@163.com>
2014-12-05 22:10:14 +0800

    [oujf][vim]1.add ccglue install, 2.add cctree.out file create at tags, 3.rename vimdir_tar.sh, 4. full path for cscope.

---
+++ scripts/vimdir_tar.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CURDATE=`date +%F`
+echo $CURDATE
+CURTIME=`date +%H%M%S`
+echo $CURTIME
+
+cd ~
+rm -rf _vim
+rm -rf .vim/.swap/*
+rm -rf .vim/.undodir/*
+cp -rf .vim _vim
+rm -vf vim_plugins*.tgz
+tar zcvf vim_plugins_$CURDATE\_$CURTIME.tgz _vim/
+rm -rf _vim
